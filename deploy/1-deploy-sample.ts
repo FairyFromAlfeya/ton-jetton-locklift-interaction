0013788da42f2ddcb547df8ec63283c90c667a06
Alexander Kunekov <fairyfromalfeya@gmail.com>
2024-08-30 16:33:03 +0200

    feat: jetton 2.0

--- deploy/1-deploy-sample.ts
+++ deploy/1-deploy-sample.ts
@@ -2,11 +2,13 @@ import { getRandomNonce, toNano } from "locklift";
 
 import MinterCode from "../jetton-contracts/jetton-minter.compiled.json";
 import WalletCode from "../jetton-contracts/jetton-wallet.compiled.json";
+import PlatformCode from "../jetton-contracts/jetton-platform.compiled.json";
 
 export default async (): Promise<void> => {
   const owner = locklift.deployments.getAccount("OwnerWallet");
   const minterCode = Buffer.from(MinterCode.hex, "hex").toString("base64");
   const walletCode = Buffer.from(WalletCode.hex, "hex").toString("base64");
+  const platformCode = Buffer.from(PlatformCode.hex, "hex").toString("base64");
 
   await locklift.deployments.deploy({
     deployConfig: {
@@ -17,6 +19,7 @@ export default async (): Promise<void> => {
         _initialOwner: owner.account.address,
         _initialMinterCode: minterCode,
         _initialWalletCode: walletCode,
+        _initialPlatformCode: platformCode,
       },
       value: toNano("1.3"),
     },
--- test/sample.spec.ts
+++ test/sample.spec.ts
@@ -1,7 +1,13 @@
 import { expect } from "chai";
-import { Address, Contract, toNano } from "locklift";
+import { Address, Contract, toNano, WalletTypes } from "locklift";
 
 import { SampleAbi } from "../build/factorySource";
+
+import WalletCode from "../jetton-contracts/jetton-wallet.compiled.json";
+import MinterCode from "../jetton-contracts/jetton-minter.compiled.json";
+
+const WALLET_CODE = Buffer.from(WalletCode.hex, "hex").toString("base64");
+const MINTER_CODE = Buffer.from(MinterCode.hex, "hex").toString("base64");
 
 describe("Test Sample contract", () => {
   let sample: Contract<SampleAbi>;
@@ -14,7 +20,7 @@ describe("Test Sample contract", () => {
     sample = locklift.deployments.getContract("Sample");
   });
 
-  describe("functions", () => {
+  describe("minter", () => {
     it("deploy()", async () => {
       const { traceTree } = await locklift.tracing.trace(
         sample.methods.deployMinter({ _callId: 555 }).send({ from: owner, amount: toNano(1.2), bounce: true }),
@@ -84,6 +90,78 @@ describe("Test Sample contract", () => {
         .and.not.to.call("excesses");
     });
 
+    it("setMeta()", async () => {
+      const { traceTree } = await locklift.tracing.trace(
+        sample.methods
+          .setMeta({ _callId: 0, _newMeta: "te6ccgEBAQEAAgAAAA==" })
+          .send({ from: owner, amount: toNano(0.05), bounce: true }),
+      );
+
+      return expect(traceTree).to.call("setMeta").count(1);
+    });
+
+    it("setWalletCode()", async () => {
+      const { traceTree } = await locklift.tracing.trace(
+        sample.methods
+          .setWalletCode({ _callId: 654, _newWalletCode: WALLET_CODE })
+          .send({ from: owner, amount: toNano(0.05), bounce: true }),
+      );
+
+      return expect(traceTree).to.call("setWalletCode").count(1);
+    });
+
+    it("upgradeMinter()", async () => {
+      const { traceTree } = await locklift.tracing.trace(
+        sample.methods
+          .upgradeMinter({ _callId: 432, _newMinterCode: MINTER_CODE })
+          .send({ from: owner, amount: toNano(0.5), bounce: true }),
+      );
+
+      return expect(traceTree).to.call("upgradeMinter").count(1);
+    });
+
+    it("drain()", async () => {
+      const minter = await sample.getFields().then(f => f.fields?.minter);
+
+      const { traceTree } = await locklift.tracing.trace(
+        sample.methods
+          .drain({ _minterOrWallet: minter!, _callId: 493, _remainingGasTo: owner })
+          .send({ from: owner, amount: toNano(0.1), bounce: true }),
+      );
+
+      return expect(traceTree).to.call("drain").count(1);
+    });
+
+    it("setAdmin()", async () => {
+      const { traceTree } = await locklift.tracing.trace(
+        sample.methods
+          .setAdmin({ _callId: 0, _newAdmin: owner })
+          .send({ from: owner, amount: toNano(0.05), bounce: true }),
+      );
+
+      return expect(traceTree).to.call("setAdmin").count(1);
+    });
+
+    it("deployWallet()", async () => {
+      const signer = await locklift.keystore.getSigner("3");
+
+      const { account } = await locklift.factory.accounts.addNewAccount({
+        type: WalletTypes.EverWallet,
+        publicKey: signer!.publicKey,
+        value: toNano(2),
+      });
+
+      const { traceTree } = await locklift.tracing.trace(
+        sample.methods
+          .deployWallet({ _callId: 192, _walletOwner: account.address })
+          .send({ from: owner, amount: toNano(0.05), bounce: true }),
+      );
+
+      return expect(traceTree).to.call("deployWallet").count(1);
+    });
+  });
+
+  describe("wallet", () => {
     it("transfer() with gas refund", async () => {
       const { traceTree } = await locklift.tracing.trace(
         sample.methods
@@ -134,8 +212,9 @@ describe("Test Sample contract", () => {
           .burn({
             _callId: 123,
             _amount: toNano(100),
-            _remainingGasTo: sample.address,
+            _callbackTo: sample.address,
             _payload: null,
+            _remainingGasTo: null,
           })
           .send({ from: owner, amount: toNano(0.05), bounce: true }),
       );
@@ -160,8 +239,9 @@ describe("Test Sample contract", () => {
           .burn({
             _callId: 888,
             _amount: toNano(100),
-            _remainingGasTo: sample.address,
+            _callbackTo: sample.address,
             _payload: payload.boc,
+            _remainingGasTo: null,
           })
           .send({ from: owner, amount: toNano(0.05), bounce: true }),
       );
@@ -180,24 +260,64 @@ describe("Test Sample contract", () => {
         });
     });
 
-    it("setMeta()", async () => {
+    it("burn() with callback and remaining gas to", async () => {
+      const payload = await locklift.provider.packIntoCell({
+        abiVersion: "2.3",
+        structure: [{ name: "remainingGasTo", type: "address" }] as const,
+        data: { remainingGasTo: owner },
+      });
+
       const { traceTree } = await locklift.tracing.trace(
         sample.methods
-          .setMeta({ _callId: 0, _newMeta: "te6ccgEBAQEAAgAAAA==" })
+          .burn({
+            _callId: 47,
+            _amount: toNano(100),
+            _callbackTo: sample.address,
+            _payload: payload.boc,
+            _remainingGasTo: owner,
+          })
           .send({ from: owner, amount: toNano(0.05), bounce: true }),
       );
 
-      return expect(traceTree).to.call("setMeta").count(1);
+      return expect(traceTree)
+        .to.call("burn")
+        .count(1)
+        .withNamedArgs({ _callId: "47" })
+        .and.to.call("onAcceptTokensBurn")
+        .count(1)
+        .withNamedArgs({
+          value0: toNano(100),
+          value1: sample.address,
+          value3: owner,
+          _payload: payload.boc,
+        });
     });
 
-    it("setAdmin()", async () => {
+    it("upgradeWallet()", async () => {
       const { traceTree } = await locklift.tracing.trace(
         sample.methods
-          .setAdmin({ _callId: 0, _newAdmin: owner })
-          .send({ from: owner, amount: toNano(0.05), bounce: true }),
+          .upgradeWallet({ _callId: 101, _remainingGasTo: sample.address })
+          .send({ from: owner, amount: toNano(0.08), bounce: true }),
       );
 
-      return expect(traceTree).to.call("setAdmin").count(1);
+      return expect(traceTree)
+        .to.call("upgradeWallet")
+        .count(1)
+        .and.to.call("excesses")
+        .count(1)
+        .withNamedArgs({ value0: "101" });
+    });
+
+    it("drain()", async () => {
+      const wallet = await sample.getFields().then(f => f.fields?.wallet);
+
+      const { traceTree } = await locklift.tracing.trace(
+        sample.methods
+          .drain({ _minterOrWallet: wallet!, _callId: 493, _remainingGasTo: owner })
+          .send({ from: owner, amount: toNano(0.1), bounce: true }),
+      );
+
+      return expect(traceTree).to.call("drain").count(1);
     });
   });
 });
